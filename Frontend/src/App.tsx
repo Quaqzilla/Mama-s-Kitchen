bcfc26390f0be82a97227df7f6b40d86f1e312e9
Quaqzilla <bbrigett207@gmail.com>
2025-09-02 11:46:21 +0200

    update to user functionality and page changing processes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,13 +6,23 @@ import { PastOrders } from './Components/PastOrders'
 import { LoginPage } from './Components/LoginPage'
 import { Selected } from './Components/Selected'
 import { CheckOut } from './Components/CheckOut'
+import {BrowserRouter, Routes, Route} from 'react-router'
 
 function App() {
 
   return (
-    <div>
-      <Landing/>
-    </div>
+    <BrowserRouter>
+      {/*Where there is 'user' replace that with the user's name from the firebase database*/}
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/admin?' element={<Admin/>}/>
+          <Route path='/userAccount' element={<Account/>}/>
+          <Route path='/user-previous-orders' element={<PastOrders/>}/>
+          <Route path='/loginPage' element={<LoginPage/>}/>
+          <Route path='/viewCategory' element={<Selected/>}/>
+          <Route path='/user-checkout' element={<CheckOut/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
--- Frontend/src/Pages/Bottom/Bottom.tsx
+++ Frontend/src/Pages/Bottom/Bottom.tsx
@@ -2,28 +2,68 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import HomeFilledIcon from '@mui/icons-material/HomeFilled';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import {useNavigate} from 'react-router-dom';
 
 
 export function Bottom(){
+
+    let navigate = useNavigate();
+
+    const home = () => {
+
+        try{
+            navigate('/');
+        }catch (error){
+            console.error(error);
+        }
+    };
+
+    const past = () => {
+
+        try{
+            navigate('/user-previous-orders');
+        }catch (error){
+            console.error(error);
+        }
+    };
+
+    const cart = () => {
+
+        try{
+            navigate('/user-checkout');
+        }catch (error){
+            console.error(error);
+        }
+    };
+
+    const account = () => {
+
+        try{
+            navigate('/userAccount');
+        }catch (error){
+            console.error(error);
+        }
+    };
+
     return(
         <div className="fixed bottom-0 p-4 bg-gray-300 w-screen flex items-center justify-between rounded-t-2xl z-30">
             <div className="flex flex-col items-center">
-                <HomeFilledIcon sx={{ fontSize: 40}}/>
+                <HomeFilledIcon sx={{ fontSize: 40}} onClick={home}/>
                 <p>Home</p>
             </div>
 
             <div className="flex flex-col items-center">
-                <ShoppingBagIcon sx={{ fontSize: 40}}/>
+                <ShoppingBagIcon sx={{ fontSize: 40}} onClick={past}/>
                 <p>Orders</p>
             </div>
 
             <div className="flex flex-col items-center">
-                <ShoppingCartIcon sx={{ fontSize: 40}}/>
+                <ShoppingCartIcon sx={{ fontSize: 40}} onClick={cart}/>
                 <p>Cart</p>
             </div>
 
             <div className="flex flex-col items-center">
-                <AccountCircleIcon sx={{ fontSize: 40}}/>
+                <AccountCircleIcon sx={{ fontSize: 40}} onClick={account}/>
                 <p>Account</p>
             </div>
         </div>
